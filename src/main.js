ec2dda54a3619cb3c688d3901c13a4b4f9f9f73b
Fabian Binkert <fabian.binkert@tngtech.com>
2024-01-26 07:50:38 +0100

    add info to main.js

--- src/main.js
+++ src/main.js
@@ -4,25 +4,23 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Fabian Binkert',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Frequent product updates.', 'Lots of automation.'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'GitHub Actions',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'I want to learn about Jenkins.'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'running and hiking in the mountains'
 		]
 	}
 });
